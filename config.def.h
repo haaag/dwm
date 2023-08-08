d97afdbd0fe317220ff346a9932373e3a2f46df1
haaag <81921095+haaag@users.noreply.github.com>
2023-08-08 20:57:20 -0300

    feat: add `quit menu` with more options

--- config.def.h
+++ config.def.h
@@ -81,6 +81,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *dwmquit[] = { "dmenu-prompt", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -125,6 +126,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,						XK_q,      spawn,          {.v = dwmquit } },
 };
 
 /* button definitions */
