68f3712dbe04ade096606907e754fb812ec824f4
haaag <81921095+haaag@users.noreply.github.com>
2023-08-15 11:19:09 -0300

    style: format lines

--- config.def.h
+++ config.def.h
@@ -108,8 +108,11 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { 
+    "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf",
+    normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL 
+};
+static const char *termcmd[]  = { TERMINAL, NULL };
 static const char *dwmquit[] = { "dmenu-prompt", NULL};
 
 static const Key keys[] = {
